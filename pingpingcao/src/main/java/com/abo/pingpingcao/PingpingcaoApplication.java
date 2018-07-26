1c8341b02c1fd85aa2e52c22a28f5a908807e672
andychen93 <413940119@qq.com>
2018-07-27 00:06:58 +0800

    add by chenli for pingpingcao

---
+++ pingpingcao/src/main/java/com/abo/pingpingcao/PingpingcaoApplication.java
@@ -0,0 +1,12 @@
+package com.abo.pingpingcao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingpingcaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingpingcaoApplication.class, args);
+	}
+}
---
+++ pingpingcao/src/test/java/com/abo/pingpingcao/PingpingcaoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abo.pingpingcao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PingpingcaoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
